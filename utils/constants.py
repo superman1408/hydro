b0acc59d7f9c38096faa04904d198ab8c70de028
Farheen Kaifee <farheenkaifee555@gmail.com>
2026-09-18 15:58:50 +0530

    gui is added

---
+++ utils/constants.py
@@ -0,0 +1,12 @@
+__version__ = "0.0.1"
+
+print(f"Loading contants variables")
+
+constant = {
+    "g": 9.81,  # Acceleration due to gravity (m/s^2)
+    "p0": 101325,  # Atmospheric pressure at sea level (Pa)
+    "rho_water": 1000,  # Density of water (kg/m^3)
+    "rho_air": 1.225,  # Density of air (kg/m^3)
+    "rho_ice": 917,  # Density of ice (kg/m^3)
+    "rho_seawater": 1025,  # Density of seawater (kg/m^3)
+}
